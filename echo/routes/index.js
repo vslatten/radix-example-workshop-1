f0e3d16a3e99a824788f184125f2ebaafd3576a0
Lars Kåre Skjørestad <lars.kaare@skjorestad.com>
2019-01-30 14:14:55 +0100

    Log response in echo

--- echo/routes/index.js
+++ echo/routes/index.js
@@ -16,6 +16,7 @@ router.get('/', function(req, res) {
     };
 
     res.set('Content-Type', 'application/json');
+    console.log('Sending response ', responseObject);
     res.status(200).send(responseObject);
 });
 
